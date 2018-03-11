afb2309bf9c838d9fe65e4bf5498d69ff72923e3
Unknown <chagnon.s21@gmail.com>
2018-03-11 17:55:00 -0400

    Initialized server

---
+++ server/src/config.ts
@@ -0,0 +1,6 @@
+// Database
+export const DATABASE_URL = '54.39.23.143';
+export const DATABASE_PORT = 27017;
+export const DATABASE_DB = 'capn';
+export const DATABASE_USER = 'server';
+export const DATABASE_PASSWORD = 'tJiam24pAl';
---
+++ server/src/index.ts
@@ -0,0 +1,52 @@
+import { Response, Request, NextFunction, Express } from 'express';
+import { readFileSync } from 'fs';
+import { json } from 'body-parser';
+import * as express from 'express';
+import * as http from 'http';
+import * as https from 'https';
+import * as cors from 'cors';
+
+import { Database } from './database';
+
+const port = 5000;
+
+Database.getInstance().then(database => {
+    if (database.connection) {
+        startServer(process.argv.slice(2)[0] == 'https');
+    } else {
+        console.log('Error while starting the server. Exiting now.');
+    }
+})
+
+function startServer(isHttps: boolean): https.Server | http.Server {
+    const app = express();
+    config(app);
+    routes(app);
+
+    let server: https.Server | http.Server;
+    if (isHttps) {
+        const options = {
+            key: readFileSync('/etc/letsencrypt/live/poutineqc.ca/privkey.pem'),
+            cert: readFileSync('/etc/letsencrypt/live/poutineqc.ca/fullchain.pem')
+        };
+    
+        server = https.createServer(options, app).listen(port);
+    } else {
+        server = http.createServer(app).listen(port);
+    }
+
+    console.log(`Server started on port: ${port}`);
+    return server;
+}
+
+function config(app: Express): void {
+    app.use(cors());
+    app.use(json());
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        console.log(`${Date.now()}- ${req.method} request from ${req.connection.remoteAddress} at ${req.originalUrl}`);
+        next();
+    });
+}
+
+function routes(app: Express): void {
+}
